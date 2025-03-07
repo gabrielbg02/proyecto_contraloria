91028f366a8164a6efef7800b874664eea0aac58
gabrielbg02 <gabrielbg0208@gmail.com>
2025-03-07 12:01:09 -0500

    act

--- app_fastapi.py
+++ app_fastapi.py
@@ -1,12 +1,72 @@
 from fastapi import FastAPI, Request, Form, staticfiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from pydantic import BaseModel
 import os
 
 app = FastAPI()
-app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static") # para mostrar la imagen
-templates = Jinja2Templates(directory="templates") 
+app.mount("/static", staticfiles.StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+MONGODB_URI = "mongodb://localhost:27017/" 
+client = MongoClient(MONGODB_URI)
+
+try:
+    client.admin.command('ping')
+    print("Conexión a MongoDB exitosa")
+except ConnectionFailure:
+    print("No se pudo conectar a MongoDB")
+
+db = client.contraloria
+formulario = db.formulario
+
+class datos_formulario(BaseModel):
+    nombre: str
+    correo: str
+    cargo: str
+    numero_cedula: str
+    numero_telefono_jefe: str
+    estado: str
+    municipio: str
+    nombre_organismo: str
+    instancia: str
+    cantidad_denuncias: str
+    cantidad_reclamos: str
+    cantidad_quejas: str
+    cantidad_peticiones: str
+    cantidad_sugerencias: str
+    cantidad_asesorias: str
+    cantidad_poblacion_masc: str
+    cantidad_poblacion_fem: str
+    cantidad_talleres_oipp: str
+    cantidad_charlas_oipp: str
+    cantidad_conversatorios_oipp: str
+    cantidad_jornadas_oipp: str
+    cantidad_forochats_oipp: str
+    cantidad_adulto_masculino_atentido_oipp: str
+    cantidad_adulto_femenino_atentida_oipp: str
+    nombre_escuela_se: str
+    cantidad_actividades_se: str
+    cantidad_talleres_se: str
+    cantidad_charlas_se: str
+    cantidad_conversatorios_se: str
+    cantidad_jornadas_se: str
+    cantidad_forochats_se: str
+    cantidad_ninosyadol_masculino_se: str
+    cantidad_ninasyadol_femenino_se: str
+    cantidad_adultos_masculino_atendidos_se: str
+    cantidad_adultos_femenino_atendidos_se: str
+    nombre_ministerio_ap: str
+    cantidad_actividades_ap: str
+    cantidad_talleres_ap: str
+    cantidad_charlas_ap: str
+    cantidad_jornadas_ap: str
+    cantidad_forochats_ap: str
+    cantidad_funcionarios_masculino_ap: str
+    cantidad_funcionarios_femenino_ap: str
 
 # Ruta para mostrar el formulario
 @app.get("/", response_class=HTMLResponse)
@@ -35,7 +95,7 @@ async def procesar_formulario(
     cantidad_poblacion_masc: str = Form(...),
     cantidad_poblacion_fem: str = Form(...),
     cantidad_talleres_oipp: str = Form(...),
-    cantidad_charlas_oipp: str = Form (...),
+    cantidad_charlas_oipp: str = Form(...),
     cantidad_conversatorios_oipp: str = Form(...),
     cantidad_jornadas_oipp: str = Form(...),
     cantidad_forochats_oipp: str = Form(...),
@@ -59,58 +119,53 @@ async def procesar_formulario(
     cantidad_jornadas_ap: str = Form(...),
     cantidad_forochats_ap: str = Form(...),
     cantidad_funcionarios_masculino_ap: str = Form(...),
-    cantidad_funcionarios_femenino_ap: str = Form(...)):
-    datos_guardar = "\n".join([
-        f"nombre: {nombre}",
-        f"correo: {correo}",
-        f"cargo: {cargo}",
-        f"numero_cedula: {numero_cedula}",
-        f"numero_telefono_jefe: {numero_telefono_jefe}",
-        f"estado: {estado}",
-        f"municipio: {municipio}",
-        f"nombre_organismo: {nombre_organismo}",
-        f"instancia: {instancia}",
-        f"cantidad_denuncias: {cantidad_denuncias}",
-        f"cantidad_reclamos: {cantidad_reclamos}",
-        f"cantidad_quejas: {cantidad_quejas}",
-        f"cantidad_peticiones: {cantidad_peticiones}",
-        f"cantidad_sugerencias: {cantidad_sugerencias}",
-        f"cantidad_asesorias: {cantidad_asesorias}",
-        f"cantidad_poblacion_masc: {cantidad_poblacion_masc}",
-        f"cantidad_poblacion_fem: {cantidad_poblacion_fem}",
-        f"cantidad_talleres_oipp: {cantidad_talleres_oipp}",
-        f"cantidad_charlas_oipp: {cantidad_charlas_oipp}",
-        f"cantidad_conversatorios_oipp: {cantidad_conversatorios_oipp}",
-        f"cantidad_jornadas_oipp: {cantidad_jornadas_oipp}",
-        f"cantidad_forochats_oipp: {cantidad_forochats_oipp}",
-        f"cantidad_adulto_masculino_atentido_oipp: {cantidad_adulto_masculino_atentido_oipp}",
-        f"cantidad_adulto_femenino_atentida_oipp: {cantidad_adulto_femenino_atentida_oipp}",
-        f"nombre_escuela_se: {nombre_escuela_se}",
-        f"cantidad_actividades_se: {cantidad_actividades_se}",
-        f"cantidad_talleres_se: {cantidad_talleres_se}",
-        f"cantidad_charlas_se: {cantidad_charlas_se}",
-        f"cantidad_conversatorios_se: {cantidad_conversatorios_se}",
-        f"cantidad_jornadas_se: {cantidad_jornadas_se}",
-        f"cantidad_forochats_se: {cantidad_forochats_se}",
-        f"cantidad_ninosyadol_masculino_se: {cantidad_ninosyadol_masculino_se}",
-        f"cantidad_ninasyadol_femenino_se: {cantidad_ninasyadol_femenino_se}",
-        f"cantidad_adultos_masculino_atendidos_se: {cantidad_adultos_masculino_atendidos_se}",
-        f"cantidad_adultos_femenino_atendidos_se: {cantidad_adultos_femenino_atendidos_se}",
-        f"nombre_ministerio_ap: {nombre_ministerio_ap}",
-        f"cantidad_actividades_ap: {cantidad_actividades_ap}",
-        f"cantidad_talleres_ap: {cantidad_talleres_ap}",
-        f"cantidad_charlas_ap: {cantidad_charlas_ap}",
-        f"cantidad_jornadas_ap: {cantidad_jornadas_ap}",
-        f"cantidad_forochats_ap: {cantidad_forochats_ap}",
-        f"cantidad_funcionarios_masculino_ap: {cantidad_funcionarios_masculino_ap}",
-        f"cantidad_funcionarios_femenino_ap: {cantidad_funcionarios_femenino_ap}"
-    ])
-
-    # Guardar los datos en un archivo
-    ruta_archivo = os.path.join(os.getcwd(), 'datos_formulario.txt')
-    with open(ruta_archivo, 'a') as archivo:
-        archivo.write(datos_guardar + "\n\n")
-
-    return templates.TemplateResponse('exito.html' , {"request": request})
-
-
+    cantidad_funcionarios_femenino_ap: str = Form(...),
+):
+    datos_guardar = {
+        "nombre": nombre,
+        "correo": correo,
+        "cargo": cargo,
+        "numero_cedula": numero_cedula,
+        "numero_telefono_jefe": numero_telefono_jefe,
+        "estado": estado,
+        "municipio": municipio,
+        "nombre_organismo": nombre_organismo,
+        "instancia": instancia,
+        "cantidad_denuncias": cantidad_denuncias,
+        "cantidad_reclamos": cantidad_reclamos,
+        "cantidad_quejas": cantidad_quejas,
+        "cantidad_peticiones": cantidad_peticiones,
+        "cantidad_sugerencias": cantidad_sugerencias,
+        "cantidad_asesorias": cantidad_asesorias,
+        "cantidad_poblacion_masc": cantidad_poblacion_masc,
+        "cantidad_poblacion_fem": cantidad_poblacion_fem,
+        "cantidad_talleres_oipp": cantidad_talleres_oipp,
+        "cantidad_charlas_oipp": cantidad_charlas_oipp,
+        "cantidad_conversatorios_oipp": cantidad_conversatorios_oipp,
+        "cantidad_jornadas_oipp": cantidad_jornadas_oipp,
+        "cantidad_forochats_oipp": cantidad_forochats_oipp,
+        "cantidad_adulto_masculino_atentido_oipp": cantidad_adulto_masculino_atentido_oipp,
+        "cantidad_adulto_femenino_atentida_oipp": cantidad_adulto_femenino_atentida_oipp,
+        "nombre_escuela_se": nombre_escuela_se,
+        "cantidad_actividades_se": cantidad_actividades_se,
+        "cantidad_talleres_se": cantidad_talleres_se,
+        "cantidad_charlas_se": cantidad_charlas_se,
+        "cantidad_conversatorios_se": cantidad_conversatorios_se,
+        "cantidad_jornadas_se": cantidad_jornadas_se,
+        "cantidad_forochats_se": cantidad_forochats_se,
+        "cantidad_ninosyadol_masculino_se": cantidad_ninosyadol_masculino_se,
+        "cantidad_ninasyadol_femenino_se": cantidad_ninasyadol_femenino_se,
+        "cantidad_adultos_masculino_atendidos_se": cantidad_adultos_masculino_atendidos_se,
+        "cantidad_adultos_femenino_atendidos_se": cantidad_adultos_femenino_atendidos_se,
+        "nombre_ministerio_ap": nombre_ministerio_ap,
+        "cantidad_actividades_ap": cantidad_actividades_ap,
+        "cantidad_talleres_ap": cantidad_talleres_ap,
+        "cantidad_charlas_ap": cantidad_charlas_ap,
+        "cantidad_jornadas_ap": cantidad_jornadas_ap,
+        "cantidad_forochats_ap": cantidad_forochats_ap,
+        "cantidad_funcionarios_masculino_ap": cantidad_funcionarios_masculino_ap,
+        "cantidad_funcionarios_femenino_ap": cantidad_funcionarios_femenino_ap,
+    }
+    # Guardar los datos en MongoDB
+    formulario.insert_one(datos_guardar)
+    return templates.TemplateResponse("exito.html", {"request": request})
